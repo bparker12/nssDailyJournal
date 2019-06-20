23f57d2caef84c76c6368f697319844cfcfc81fc
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-06-20 12:40:37 -0500

    Modularized code and included new loop to pull in API data to DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,43 +1,84 @@
-/*Define the keys and value for a JavaScript object that represents
-a journal entry about what you learned today*/
-const collection = []
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
 
-collection.push(journalEntry)
-console.log(collection)
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+API.getJournalEntries().then(loopToDom)
 
-function journalText(date, concepts, entry, mood) {
-    return `
-    <div class="entriesCSS">
-        <h1 class="jDate">Today's Date: ${date}</h1>
-        <h2 class="jConcepts">Today we learned: ${concepts}
-        <form>
-        <textarea
-        rows=""
-        columns="25"
-        name="conceptsCovered"
-        id="conceptsCovered"
-        class="label">${entry}</textarea>
-        </form>
-        <p class = "jmood"> My mood for today is: ${mood}</p>
-    </div>`
-}
-const journalContainer = document.querySelector(".entryLog")
+
+// API.getJournalEntries()
+//     .then (entries => {
+//         // entries.forEach(element => {
+//         journalContainer.innerHTML += journalText(
+//             entries.date,
+//             entries.concepts_covered,
+//             entries.journal_entry,
+//             entries.mood
+//         )
+//     })
 
 
 
+// console.log()
+// for (let i = 0; i < entries.length; i++) {
+//     const entry = entries[i]
+//     journalContainer.innerHTML += journalText(
+//         entry.date,
+//         entry.concepts_covered,
+//         entry.journal_entry,
+//         entry.mood
+//     )
+// }
+// What should happen when we finally have the array?
 
-fetch("http://localhost:3000/entries") // Fetch from the API
-    .then(data => data.json())  // Parse as JSON
-    .then(entries => {
-        console.log()
-        for (let i = 0; i < entries.length; i++) {
-            const entry = entries[i]
-            journalContainer.innerHTML += journalText(
-                entry.date,
-                entry.concepts_covered,
-                entry.journal_entry,
-                entry.mood
-            )
-        }
-        // What should happen when we finally have the array?
-    })
+
+
+
+
+
+
+// /*Define the keys and value for a JavaScript object that represents
+// a journal entry about what you learned today*/
+// const collection = []
+
+// collection.push(journalEntry)
+// console.log(collection)
+
+// function journalText(date, concepts, entry, mood) {
+//     return `
+//     <div class="entriesCSS">
+//         <h1 class="jDate">Today's Date: ${date}</h1>
+//         <h2 class="jConcepts">Today we learned: ${concepts}
+//         <form>
+//         <textarea
+//         rows=""
+//         columns="25"
+//         name="conceptsCovered"
+//         id="conceptsCovered"
+//         class="label">${entry}</textarea>
+//         </form>
+//         <p class = "jmood"> My mood for today is: ${mood}</p>
+//     </div>`
+// }
+// const journalContainer = document.querySelector(".entryLog")
+
+
+
+
+// fetch("http://localhost:3000/entries") // Fetch from the API
+//     .then(data => data.json())  // Parse as JSON
+//     .then(entries => {
+//         console.log()
+//         for (let i = 0; i < entries.length; i++) {
+//             const entry = entries[i]
+//             journalContainer.innerHTML += journalText(
+//                 entry.date,
+//                 entry.concepts_covered,
+//                 entry.journal_entry,
+//                 entry.mood
+//             )
+//         }
+//         // What should happen when we finally have the array?
+//     })
